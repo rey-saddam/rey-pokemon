88bb500576a6f971ea8fe1dd38089ac015b20bf7
Saddam Husain <saddam.husain@rey.id>
2022-06-01 14:30:41 +0700

    refactor: Change api route to /pokedex

--- src/entity/constant/api.ts
+++ src/entity/constant/api.ts
@@ -1,10 +1,9 @@
 export enum API_ROUTE {
-    AUTH = '/auth',
-    PROFILE = '/profile',
-    POST = '/post'
+    POKEDEX = '/pokedex'
 }
 
-export const PROTO_FILE_PATH = './node_modules/rey-common-model/proto/rey_default.proto';
+export const PROTO_FILE_PATH =
+    './node_modules/rey-common-model/proto/rey_default.proto';
 
 export default {
     API_ROUTE
