8f1932f5f95492e96ee997ac8107f0e2717cf236
Saddam Husain <saddam.husain@rey.id>
2022-06-01 14:35:42 +0700

    feat: Add update pokedex logic and route

--- src/controllers/pokedex_controller.ts
+++ src/controllers/pokedex_controller.ts
@@ -20,11 +20,21 @@ export class PokedexController extends BaseController {
         return await this.pokedexService.createPokedex(data.body);
     }
 
+    public async updatePokedex(data: RequestData): Promise<any> {
+        const pokedexId = data.params.id;
+        const pokedexData = data.body;
+
+        return await this.pokedexService.updatePokedex(pokedexId, pokedexData);
+    }
+
     public setRoutes(): void {
         this.addRoute('get', '/', this.getAllPokedex.bind(this));
         this.addRoute('get', '/:id', this.getPokedexById.bind(this));
         this.addRoute('post', '/', this.createPokedex.bind(this), {
             validate: SCHEME.CREATE_POKEDEX
+        });
+        this.addRoute('put', '/:id', this.updatePokedex.bind(this), {
+            validate: SCHEME.UPDATE_POKEDEX
         });
     }
 }
