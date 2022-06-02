c3c57b51e0f6d57c73be7ad905b70525423b7a7f
Saddam Husain <saddam.husain@rey.id>
2022-06-02 10:59:11 +0700

    feat: Create constant pokedex variable for testing

---
+++ tests/variables/pokedex_variable.ts
@@ -0,0 +1,23 @@
+export const POKEDEX = {
+    id: '6b86242c-bd2b-4c7e-9c41-6c5fbfd7ef086b86242',
+    name: 'Pickachu',
+    description:
+        'Pikachu is an Electric type Pokémon introduced in Generation 1. It is known as the Mouse Pokémon',
+    image: 'http://api.pokemon.dev/images/pikachu.jpg',
+    types: ['Electric'],
+    category: 'Mouse Pokémon',
+    height: 0.4,
+    weight: 6,
+    abilities: ['Static', 'Lightning Rod'],
+    weaknesses: ['Ground'],
+    hp: 35,
+    attack: 55,
+    defense: 40,
+    special_attack: 50,
+    special_defense: 50,
+    speed: 90,
+    evolutions: ['Pikachu', 'Raichu'],
+    created_at: '2020-01-01T00:00:00.000Z',
+    updated_at: '2020-01-01T00:00:00.000Z',
+    deleted_at: null
+};
